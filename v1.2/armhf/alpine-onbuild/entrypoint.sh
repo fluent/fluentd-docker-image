52654b17e28cccafe64ef208b564658d7f50f9e0
Evan Lezar <evanlezar@gmail.com>
2018-06-29 11:45:14 +0200

    Add ARM images
    * Add templating for ARM images to Dockerfile template
    * Enable cross-building of ARM images
    * Add a separate list of ARM targets to Makefile
    
    Signed-off-by: Evan Lezar <mail@evanlezar.com>

---
+++ v1.2/armhf/alpine-onbuild/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/dumb-init /bin/sh
+
+uid=${FLUENT_UID:-1000}
+
+# check if a old fluent user exists and delete it
+cat /etc/passwd | grep fluent
+if [ $? -eq 0 ]; then
+    deluser fluent
+fi
+
+# (re)add the fluent user with $FLUENT_UID
+adduser -D -g '' -u ${uid} -h /home/fluent fluent
+
+# chown home and data folder
+chown -R fluent /home/fluent
+chown -R fluent /fluentd
+
+exec su-exec fluent "$@"
