f9ccea41c012f19d249e7c8ea36af5639e8bed75
Jordan Hamel <jordanhamel@gmail.com>
2018-08-16 12:06:47 -0700

    sourced vars /etc/default/fluentd
    Signed-off-by: Jordan Hamel <jordanhamel@gmail.com>

--- v1.2/debian/entrypoint.sh
+++ v1.2/debian/entrypoint.sh
@@ -2,6 +2,15 @@
 
 uid=${FLUENT_UID:-1000}
 
+#source vars from /etc/default/fluentd
+DEFAULT=/etc/default/fluent
+
+if [ -r $DEFAULT ]; then
+    set -o allexport
+    source $DEFAULT
+    set +o allexport
+fi
+
 # check if a old fluent user exists and delete it
 cat /etc/passwd | grep fluent
 if [ $? -eq 0 ]; then
