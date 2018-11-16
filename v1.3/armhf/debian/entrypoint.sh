23e6e926c8ceba0e2194030afb373a4d1c7302f3
Lee Jones <lee.jones@linaro.org>
2018-11-16 09:52:36 +0000

    Have the user append arguments on the end of the 'docker run' command
    This allows us to remove the `FLUENTD_CONF` and `FLUENTD_OPT` envs.
    
    As suggested by @tianon
    
      LINK: https://github.com/docker-library/official-images/pull/3724#issuecomment-355423609
    
    This is part of an effort to make FluentD part of Docker's Official Images.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- v1.3/armhf/debian/entrypoint.sh
+++ v1.3/armhf/debian/entrypoint.sh
@@ -9,4 +9,20 @@ if [ -r $DEFAULT ]; then
     set +o allexport
 fi
 
+# If the user has supplied only arguments append them to `fluentd` command
+if [ "${1#-}" != "$1" ]; then
+    set -- fluentd "$@"
+fi
+
+# If user does not supply config file or plugins, use the default
+if [ "$1" = "fluentd" ]; then
+    if ! echo $@ | grep ' \-c' ; then
+       set -- "$@" -c /fluentd/etc/fluent.conf
+    fi
+
+    if ! echo $@ | grep ' \-p' ; then
+       set -- "$@" -p /fluentd/plugins
+    fi
+fi
+
 exec gosu fluent "$@"
